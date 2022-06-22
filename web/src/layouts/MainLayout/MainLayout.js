361bb5a51a503f121eb889edfc06422b996fa3d5
James C. Tucker <jamesctucker@protonmail.com>
2022-06-22 09:55:01 -0500

    style Create page

--- web/src/layouts/MainLayout/MainLayout.js
+++ web/src/layouts/MainLayout/MainLayout.js
@@ -1,12 +1,12 @@
 import Navbar from 'src/components/Navbar'
 
 const MainLayout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-    </>
-  )
+   return (
+      <>
+         <Navbar />
+         <main className="p-8 flex justify-center">{children}</main>
+      </>
+   )
 }
 
 export default MainLayout
--- web/src/pages/CreatePage/CreatePage.js
+++ web/src/pages/CreatePage/CreatePage.js
@@ -16,38 +16,43 @@ const CreatePage = () => {
    return (
       <>
          <MetaTags title="Create" description="Create page" />
+         <div class="w-full md:w-3/5 lg:w-1/3">
+            <h1>You're one step closer to making someone's day!</h1>
+            <Form onSubmit={next}>
+               <Label
+                  name="receiverName"
+                  className="rw-label"
+                  errorClassName="rw-label rw-label-error"
+               >
+                  Who is this event for?
+               </Label>
+               <TextField
+                  name="receiverName"
+                  className="rw-input"
+                  errorClassName="rw-input rw-input-error"
+               />
 
-         <h1>You're one step closer to making someone's day!</h1>
-         <Form onSubmit={next}>
-            <Label
-               name="receiverName"
-               className="rw-label"
-               errorClassName="rw-label rw-label-error"
-            >
-               Who is this event for?
-            </Label>
-            <TextField
-               name="receiverName"
-               className="rw-input"
-               errorClassName="rw-input rw-input-error"
-            />
+               <Label
+                  name="eventType"
+                  className="rw-label"
+                  errorClassName="rw-label rw-label-error"
+               >
+                  What kind of event is this?
+               </Label>
+               <SelectField
+                  name="eventType"
+                  className="rw-input"
+                  multiple={false}
+               >
+                  <option>birthday</option>
+                  <option>anniversary</option>
+                  <option>get well</option>
+                  <option>memorial</option>
+               </SelectField>
 
-            <Label
-               name="eventType"
-               className="rw-label"
-               errorClassName="rw-label rw-label-error"
-            >
-               What kind of event is this?
-            </Label>
-            <SelectField name="eventType" className="rw-input" multiple={false}>
-               <option>birthday</option>
-               <option>anniversary</option>
-               <option>get well</option>
-               <option>memorial</option>
-            </SelectField>
-
-            <Submit className="btn btn-primary">Next</Submit>
-         </Form>
+               <Submit className="btn btn-primary my-6">Next</Submit>
+            </Form>
+         </div>
       </>
    )
 }
